37aff4251d94a87467d1d371727f52b21b90925f
Crowdbotics <engineering@crowdbotics.com>
2024-01-15 23:07:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-darkness-2",
+  url: "https://wandering-darkness-2.botics.co",
+  api: "https://wandering-darkness-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
